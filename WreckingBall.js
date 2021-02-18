52c61d99b4e468aa981ac5236226f780c88c8e67
shekinahekka <71063335+shekinahekka@users.noreply.github.com>
2021-02-18 21:49:08 +0530

    Add files via upload

---
+++ WreckingBall.js
@@ -0,0 +1,25 @@
+class WreckingBall {
+  constructor(x, y, width, height, angle) {
+    var options = { 
+      density: 1, 
+      frictionAir: 0.005};
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+    this.image= loadImage('images/ball.png');
+
+  }
+
+  display() {
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image,0,0,this.width, this.height);
+    //ellipse(0, 0, this.width, this.height);
+    pop();
+  }
+}
